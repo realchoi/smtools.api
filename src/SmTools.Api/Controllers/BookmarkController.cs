2c1370e180c868d5f806c1efc9fe6f571de8dddc
Eric Choi <meetcds@foxmail.com>
2023-11-02 21:56:13 +0800

    feat: 书签工具接口需要认证

--- src/SmTools.Api/Controllers/BookmarkController.cs
+++ src/SmTools.Api/Controllers/BookmarkController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SmTools.Api.Application.BookmarkCategories;
 using SmTools.Api.Application.BookmarkItems;
 using SmTools.Api.Model.BookmarkCategories.Dtos;
@@ -11,6 +12,7 @@ namespace SmTools.Api.Controllers;
 /// </summary>
 [ApiController]
 [Route("bookmark")]
+[Authorize]
 public class BookmarkController : ControllerBase
 {
     private readonly IBookmarkCategoryAppService _bookmarkCategoryAppService;
